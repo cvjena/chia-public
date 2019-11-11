d9638954af254081fd52163adf29b9ce8833014a
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-11-11 14:57:12 +0100

    Added tests for embedding invertibility

---
+++ tests/test_keras_embedding_based_hcs.py
@@ -0,0 +1,29 @@
+import unittest
+import functools
+
+from chia.methods import hierarchicalclassification
+from chia import knowledge
+
+
+class KerasEmbeddingBasedHCTestCase(unittest.TestCase):
+    def setUp(self):
+        self.kb = knowledge.KnowledgeBase()
+        self.kb.observe_concepts(["A", "B", "C"])
+
+        srs = knowledge.StaticRelationSource([("B", "A"), ("C", "A")])
+        self.kb.add_relation(
+            "hypernymy",
+            is_reflexive=False,
+            is_symmetric=False,
+            is_transitive=True,
+            sources=[srs],
+        )
+
+    def test_all_hcs(self):
+        for method_key in hierarchicalclassification.methods():
+            method = hierarchicalclassification.method(method_key, self.kb)
+            with self.subTest("Testing HC", dataset=method_key):
+                for concept in self.kb.get_observed_concepts():
+                    embedded = method.embed([concept.data["uid"]])
+                    deembedded = method.deembed(embedded)
+                    self.assertEqual(concept.data["uid"], deembedded[0])
