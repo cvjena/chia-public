6eb3722eed987ad5db90484d1767d30f20204d11
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-11-15 12:17:15 +0100

    Reduced log level to reenable error printing

--- chia/__init__.py
+++ chia/__init__.py
@@ -3,4 +3,4 @@ print("This is CHIA: Concept Hierarchies for Incremental and Active Learning")
 # Try turning off tensorflow warnings
 import os
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
