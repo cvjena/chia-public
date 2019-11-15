ab552bd04b43b5d9e9fa73a2eb48455b91a8818c
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-11-15 12:06:07 +0100

    Disable TensorFlow warnings

---
+++ chia/__init__.py
@@ -0,0 +1,7 @@
+print("This is CHIA: Concept Hierarchies for Incremental and Active Learning")
+
+# Try turning off tensorflow warnings
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import tensorflow as tf
