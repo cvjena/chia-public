3628a16e6bcc1ab58ff21ff5b87212934571c5e2
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-12-17 14:28:26 +0100

    [CHIA-14] Autodetect RAM #review

--- chia/__init__.py
+++ chia/__init__.py
@@ -4,3 +4,18 @@ print("This is CHIA: Concept Hierarchies for Incremental and Active Learning")
 import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+# GPU stuff
+from chia.framework import configuration
+try:
+    from GPUtil import GPUtil
+    import math
+    gpus = GPUtil.getGPUs()
+    assert len(gpus) == 1, "Only one GPU is supported right now."
+    gpu = gpus[0]
+    configuration.set_system("gpu0_vram", math.trunc(gpu.memoryTotal / 102.4) / 10)
+except Exception as ex:
+    print(f"Could not get amount of available VRAM: {str(ex)}. Setting default value of 4GiB.")
+    configuration.set_system("gpu0_vram", 4.0)
+
+print(f"GPU0: {configuration.get_system('gpu0_vram')} Gib VRAM")
