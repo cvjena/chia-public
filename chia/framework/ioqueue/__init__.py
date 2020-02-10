a4778232298873be4150e11fe892bdd716f63a5e
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2020-02-10 11:49:49 +0100

    ioqueue: Added niceness

--- chia/framework/ioqueue/__init__.py
+++ chia/framework/ioqueue/__init__.py
@@ -2,9 +2,14 @@ import multiprocessing
 import queue
 import threading
 import time
+from typing import Any, Callable, Generator, Union
 
 
-def make_generator_faster(gen, method, max_buffer_size=100):
+def make_generator_faster(
+    gen: Callable[..., Generator[Any, None, None]],
+    method: str,
+    max_buffer_size: int = 100,
+):
     if method == "threading":
         return make_generator_faster_threading(gen, max_buffer_size)
     elif method == "multiprocessing":
@@ -15,59 +20,49 @@ def make_generator_faster(gen, method, max_buffer_size=100):
         raise ValueError(f"Unknown method {method}")
 
 
-def make_generator_faster_threading(gen, max_buffer_size=100):
-    def producer_thread_main(q_: queue.Queue):
-        gen_instance = gen()
-        for item in gen_instance:
-            q_.put(item)
+def _producer_main(
+    q_: Union[queue.Queue, multiprocessing.Queue],
+    gen: Callable[..., Generator[Any, None, None]],
+) -> None:
+    gen_instance = gen()
+    for item in gen_instance:
+        q_.put(item)
 
-        print("Producer done.")
-        q_.put("THE_END")
+    print("Producer done.")
+    q_.put("THE_END")
 
-    def consumer_generator(q_: queue.Queue):
-        while True:
-            try:
-                item = q_.get(timeout=1.0)
-                if item == "THE_END":
-                    print("Consumer done.")
-                    return
-                else:
-                    yield item
-            except queue.Empty:
-                print("Empty queue!")
-                time.sleep(1.0)
 
+def _consumer_generator(
+    q_: Union[queue.Queue, multiprocessing.Queue]
+) -> Generator[Any, None, None]:
+    while True:
+        try:
+            item = q_.get(timeout=1.0)
+            if item == "THE_END":
+                print("Consumer done.")
+                return
+            else:
+                yield item
+        except queue.Empty:
+            print("Empty queue!")
+            time.sleep(1.0)
+
+
+def make_generator_faster_threading(
+    gen: Callable[..., Generator[Any, None, None]], max_buffer_size: int = 100
+):
     q = queue.Queue(maxsize=max_buffer_size)
-    producer_thread = threading.Thread(target=producer_thread_main, args=(q,))
+    producer_thread = threading.Thread(target=_producer_main, args=(q, gen))
 
     producer_thread.start()
-    return consumer_generator(q)
+    return _consumer_generator(q)
 
 
-def make_generator_faster_multiprocessing(gen, max_buffer_size=100):
-    def producer_process_main(q_: multiprocessing.Queue):
-        gen_instance = gen()
-        for item in gen_instance:
-            q_.put(item)
-
-        print("Producer done.")
-        q_.put("THE_END")
-
-    def consumer_generator(q_: multiprocessing.Queue):
-        while True:
-            try:
-                item = q_.get(timeout=1.0)
-                if item == "THE_END":
-                    print("Consumer done.")
-                    return
-                else:
-                    yield item
-            except queue.Empty:
-                print("Empty queue!")
-                time.sleep(1.0)
-
+def make_generator_faster_multiprocessing(
+    gen: Callable[..., Generator[Any, None, None]], max_buffer_size: int = 100
+):
     q = multiprocessing.Queue(maxsize=max_buffer_size)
-    producer_process = multiprocessing.Process(target=producer_process_main, args=(q,))
+    producer_process = multiprocessing.Process(target=_producer_main, args=(q, gen))
 
     producer_process.start()
-    return consumer_generator(q)
+    return _consumer_generator(q)
